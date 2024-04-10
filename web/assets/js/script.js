b795caecf02deab79646fc12abf53b747a6fded3
Matias gomez <mrmatiasgomez@gmail.com>
2024-04-10 22:06:05 +0200

    Add e-mail section

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -4,50 +4,75 @@ var inputUrl = document.getElementById("inputUrl");
 var inputPhone = document.getElementById("inputPhone");
 var inputSms = document.getElementById("inputSms");
 var inputSmsBody = document.getElementById("inputSmsBody");
+var inputEmail = document.getElementById("inputEmail");
+var inputEmailSubject = document.getElementById("inputEmailSubject");
+var inputEmailBody = document.getElementById("inputEmailBody");
 
 // sms: 0750455364&body= test pour envoyer un sms
 const btnUrl = document.getElementById("btnUrl");
 const btnPhone = document.getElementById("btnPhone");
 const btnSms = document.getElementById("btnSms");
+const btnEmail = document.getElementById("btnEmail");
 
 const sectionUrl = document.getElementById("sectionUrl");
 const sectionPhone = document.getElementById("sectionPhone");
 const sectionSms = document.getElementById("sectionSms");
+const sectionEmail = document.getElementById("sectionEmail");
 
 btnUrl.addEventListener("click", function () {
   btnUrl.classList.add("active");
   btnPhone.classList.remove("active");
   btnSms.classList.remove("active");
+  btnEmail.classList.remove("active");
 
   sectionUrl.classList.remove("d-none");
   sectionUrl.classList.add("d-block");
 
   sectionPhone.classList.add("d-none");
   sectionSms.classList.add("d-none");
+  sectionEmail.classList.add("d-none");
 });
 
 btnPhone.addEventListener("click", function () {
   btnPhone.classList.add("active");
   btnUrl.classList.remove("active");
   btnSms.classList.remove("active");
+  btnEmail.classList.remove("active");
 
   sectionPhone.classList.remove("d-none");
   sectionUrl.classList.add("d-block");
 
   sectionSms.classList.add("d-none");
   sectionUrl.classList.add("d-none");
+  sectionEmail.classList.add("d-none");
 });
 
 btnSms.addEventListener("click", function () {
   btnSms.classList.add("active");
   btnUrl.classList.remove("active");
   btnPhone.classList.remove("active");
+  btnEmail.classList.remove("active");
 
   sectionSms.classList.remove("d-none");
   sectionSms.classList.add("d-block");
 
   sectionPhone.classList.add("d-none");
   sectionUrl.classList.add("d-none");
+  sectionEmail.classList.add("d-none");
+});
+
+btnEmail.addEventListener("click", function () {
+  btnEmail.classList.add("active");
+  btnSms.classList.remove("active");
+  btnUrl.classList.remove("active");
+  btnPhone.classList.remove("active");
+
+  sectionEmail.classList.remove("d-none");
+  sectionEmail.classList.add("d-block");
+
+  sectionPhone.classList.add("d-none");
+  sectionUrl.classList.add("d-none");
+  sectionSms.classList.add("d-none");
 });
 
 function generateQrCode() {
@@ -106,7 +131,6 @@ inputSmsBody.addEventListener("keyup", function (e) {
     sectionDownload.innerHTML = "";
   }
 });
-
 
 const pFooter = document.getElementById("pFooter");
 pFooter.innerHTML = new Date().getFullYear() + " - MATIAS GOMEZ";
